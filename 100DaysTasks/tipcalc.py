b03b5605ed9bb44d7b7f4e232682fb903c69aaca
SaidRafili <said.rafili08@gmail.com>
2026-07-13 23:44:16 +0400

    Tip Calculator added

---
+++ 100DaysTasks/tipcalc.py
@@ -0,0 +1,10 @@
+print("Welcome to Tip Calculator!")
+cost = float(input("How much did the meal cost you? "))
+tip = int(input("What percentage are you planning to give the staff as a tip? "))
+people = int(input("How many people are there behind the table? "))
+tipPercentage = cost / 100 * tip
+costAfterTip = tipPercentage + cost
+costPerPerson = costAfterTip / people
+print("You should pay " + str(costPerPerson))
+
+#simpler approach
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 <h1>100DaysTasks</h1>
 <ol type="1">
     <li><a href="/100DaysTasks/brandname.py">Brand Name Generator</a> - Jul 13 23:26</li>
+    <li><a href="/100DaysTasks/tipcalc.py">Tip Calculator</a> - Jul 13 23:43</li>
 </ol>
